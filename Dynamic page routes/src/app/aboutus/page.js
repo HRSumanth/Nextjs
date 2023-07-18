a6169e7ccd241d70f4096a13388719617286035d
HRSumanth <130749610+HRSumanth@users.noreply.github.com>
2023-07-18 12:52:36 +0530

    Add files via upload

---
+++ Dynamic page routes/src/app/aboutus/page.js
@@ -0,0 +1,7 @@
+const AboutUs=()=>{
+    return (
+        <h2>About Us page </h2>
+    )
+}
+
+export default AboutUs
---
+++ Dynamic page routes/src/app/news/page.js
@@ -0,0 +1,7 @@
+const News=()=>{
+    return (
+        <h2>News page </h2>
+    )
+}
+
+export default News
